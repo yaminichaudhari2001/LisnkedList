062cc291a0ba1f8348076a69a3850af973048977
yami <yaminichaudhari2001@gmail.com>
2023-07-01 21:14:30 +0530

    UC9

--- src/bridgelabz/linkedlist/RunLinkedList.java
+++ src/bridgelabz/linkedlist/RunLinkedList.java
@@ -1,13 +1,12 @@
 package bridgelabz.linkedlist;
-//Ability to insert 40 after 30 to
-//the Linked List sequence of
-//56->30->70
-//- Write Junit Test Case as demonstrated
-//in class
-//- Search LinkedList to get Node with key
-//value 30
-//- Then Insert 40 to 30
-//- Final Sequence: 56->30->40->70
+//Ability to delete 40 from the Linked
+//List sequence of 56->30->40->70
+//and show the size of LinkedList is 3
+//- Write Junit Test Case as demonstrated in class
+//- Search LinkedList to find node with key value 40
+//- Delete the node
+//- Implement size() and show the Linked List size is 3
+//- Final Sequence: 56->30->70
 public class RunLinkedList {
     public static void main(String[] args) {
         System.out.println("This is LinkedLink");
@@ -19,6 +18,11 @@ public class RunLinkedList {
         li.findNode(100);
         li.searchAndInsert(40, 30);
         li.printl();
+        System.out.println();
+        li.searchAndDelete(40);
+        li.printl();
+        System.out.println();
+        li.size();
 
 
     }
