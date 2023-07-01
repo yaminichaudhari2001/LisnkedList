025186f630805230633c33789f742e5d2f95fdc3
yami <yaminichaudhari2001@gmail.com>
2023-07-01 19:43:22 +0530

    UC3

--- src/bridgelabz/linkedlist/LinkedList.java
+++ src/bridgelabz/linkedlist/LinkedList.java
@@ -12,6 +12,17 @@ Node head;
         head = newNode;
 
     }
+    void addlast(int data) {
+        Node newnode = new Node(data);
+        if (head == null) {
+            head = newnode;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newnode;
+    }
 
     void printl() {
         Node temp = head;
--- src/bridgelabz/linkedlist/RunLinkedList.java
+++ src/bridgelabz/linkedlist/RunLinkedList.java
@@ -1,5 +1,11 @@
 package bridgelabz.linkedlist;
-
+//Ability to create Linked
+//List by appending 30 and
+//70 to 56
+//- Node with data 56 is First Created
+//- Next Append 30 to 56
+//- Finally Append 70 to 30
+//- LinkedList Sequence: 56->30->70
 public class RunLinkedList {
     public static void main(String[] args) {
         System.out.println("This is LinkedLink");
